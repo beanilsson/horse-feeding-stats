e255e923ac7362ff41f7c6505d151c4646786acf
Bea Nilsson <beatrice.nilsson@regionvasterbotten.se>
2019-08-08 17:14:24 +0200

    Change npm test script and add server test

---
+++ spec/server/serverSpec.js
@@ -0,0 +1,14 @@
+const request = require('request');
+
+const baseUrl = 'http://localhost:3000';
+
+describe('Server is alive', () => {
+    describe('GET /', () => {
+        it('returns status code 200', (done) => {
+            request.get(baseUrl + '/', (error, response, body) => {
+                expect(response.statusCode).toBe(200);
+            });
+            done();
+        });
+    });
+});
