3a8867f9eae1974d519e851b5e67e2298b2045ab
Bea Nilsson <beatrice.nilsson@regionvasterbotten.se>
2019-08-10 12:40:46 +0200

    Validation for consumption

--- models/consumptionModel.js
+++ models/consumptionModel.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 const consumptionSchema = new mongoose.Schema({
-    date: Date,
-    amount: Number,
-    unit: String,
-    fodderType: String,
-    batch: String,
+    date: { type: Date, required: true },
+    amount: { type: Number, required: true },
+    unit: { type: String, required: true },
+    batch: { type: String, required: true },
     horse: String,
-    animalGroup: String
+    animalGroup: String,
+    fodderType: { type: String, required: true } //move fodderType to batch
 });
 
 module.exports = mongoose.model('Consumption', consumptionSchema);
