13fbb7420bf94fccd599e6e23e3957c3599cbc61
Bea Nilsson <beatrice.nilsson@regionvasterbotten.se>
2019-08-21 15:24:00 +0200

    Delete animalGroup functionality

--- controllers/animalGroupController.js
+++ controllers/animalGroupController.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const AnimalGroup = require('../models/animalGroupModel');
 
 router.get('/', (req, res) => {
-    AnimalGroup.find((err, animalGroups) => {
+    AnimalGroup.find({deleted: false}, (err, animalGroups) => {
         if (err) {
             console.log(err);
             res.render('pages/error');
@@ -23,6 +23,19 @@ router.post('/', (req, res) => {
     }, (err) => {
         console.log(err);
         res.sendStatus(500);
+    });
+});
+
+router.post('/:animalGroupName', (req, res) => {
+    const animalGroupName = req.params.animalGroupName;
+
+    AnimalGroup.updateOne({name: animalGroupName}, {deleted: true}, (err) => {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+        } else {
+            res.render('pages/animalGroupDeleted');
+        }
     });
 });
 
--- models/animalGroupModel.js
+++ models/animalGroupModel.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 const animalGroupSchema = new mongoose.Schema({
     name: String,
-    amount: Number
+    amount: Number,
+    deleted: {type: Boolean, default: false},
+    deleteDate: Date //use this to exclude consumptions for animalGroups after this date
 });
 
 module.exports = mongoose.model('AnimalGroup', animalGroupSchema);
