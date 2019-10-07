2c574d12fe7825fa814d7b3b9adb411071544e7f
Bea Nilsson <beatrice.nilsson@regionvasterbotten.se>
2019-10-07 11:46:36 +0200

    Basic authentication setup

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
+const session = require('express-session');
 const app = express();
-const dbUrl = 'mongodb://localhost:27017/hfs';
 const mongoose = require('mongoose');
 const async = require('async');
 const bodyParser = require('body-parser');
@@ -9,7 +9,30 @@ const batches = require('./controllers/batchController');
 const consumptions = require('./controllers/consumptionController');
 const animalGroups = require('./controllers/animalGroupController');
 
+require('dotenv').config();
+
+const port = process.env.PORT;
+const host = process.env.HOST;
+const dbHost = process.env.DB_HOST;
+const dbPort = process.env.DB_PORT;
+const dbName = process.env.DB_NAME;
+const sessionSec = process.env.SESSION_SEC;
+const username = process.env.APP_USERNAME;
+const password = process.env.APP_PASSWORD;
+
+const dbUrl = `mongodb://${dbHost}:${dbPort}/${dbName}`;
+
+const authCheck = (req, res, next) => {
+    if (!req.session.userName) {
+        res.send('You are not authorized to view this page');
+    } else {
+        res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+        next();
+    }
+};
+
 app.set('view engine', 'ejs');
+app.use(session({secret: sessionSec, saveUninitialized: true,resave: true}));
 app.use(bodyParser.urlencoded({
     extended: false
 }));
@@ -17,15 +40,39 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
+    res.render('pages/loginPage');
+});
+
+app.use('/start', authCheck, (req, res) => {
     res.render('pages/index');
 });
 
-app.use('/batches', batches);
-app.use('/consumptions', consumptions);
-app.use('/animalGroups', animalGroups);
+app.use('/batches', authCheck, batches);
+app.use('/consumptions', authCheck, consumptions);
+app.use('/animalGroups', authCheck, animalGroups);
 
+app.get('/loginPage', (req, res) => {
+    res.render('pages/loginPage');
+});
+
+app.post('/login', (req, res) => {
+    const body = req.body;
+    if (body.userName === username && body.password === password) {
+        const session = req.session;
+        session.userName = body.userName;
+        res.redirect('/start');
+    } else {
+        res.send('Wrong username or password :(');
+    }
+});
+
+app.post('/logout', function (req, res) {
+    delete req.session.userName;
+    res.redirect('/loginPage');
+});
 
 mongoose.connect(dbUrl, {useNewUrlParser: true});
 
-app.listen(3000);
-console.log('Server listening on port 3000');
+app.listen(port, host, () => {
+    console.log(`Server running at ${host}:${port}`);
+});
